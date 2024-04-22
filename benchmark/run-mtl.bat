bd0047c6551793d3117096bc4cfc911aa9628ff1
Tan, Cheen Hau <33478814+chtanch@users.noreply.github.com>
2024-04-22 22:28:28 +0800

    Add more models to benchmark

--- benchmark/run-mtl.bat
+++ benchmark/run-mtl.bat
@@ -7,7 +7,7 @@ set CWD=%cd%
 set OUTPUT_DIR=%cd%\results\%date:~10,4%-%date:~4,2%-%date:~7,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
 mkdir "%OUTPUT_DIR%"
 
-REM Ensure no csv files in all-in-one folder
+@REM Ensure no csv files in all-in-one folder
 cd ..\python\llm\dev\benchmark\all-in-one
 if exist *.csv (
     echo csv file exists. Stopping.
@@ -15,11 +15,11 @@ if exist *.csv (
 )
 cd %CWD%
 
-REM set environment variables
+@REM set environment variables
 set SYCL_CACHE_PERSISTENT=1
 set BIGDL_LLM_XMX_DISABLED=1
 
-REM transformers==4.31.0
+@REM transformers==4.31.0
 python -m pip install transformers==4.31.0
 copy config_431.yaml ..\python\llm\dev\benchmark\all-in-one\config.yaml
 cd ..\python\llm\dev\benchmark\all-in-one
@@ -27,7 +27,15 @@ python run.py
 move *.csv "%OUTPUT_DIR%"
 cd %CWD%
 
-REM transformers==4.37.0
+@REM transformers==4.34.0
+python -m pip install transformers==4.34.0
+copy config_434.yaml ..\python\llm\dev\benchmark\all-in-one\config.yaml
+cd ..\python\llm\dev\benchmark\all-in-one
+python run.py
+move *.csv "%OUTPUT_DIR%"
+cd %CWD%
+
+@REM transformers==4.37.0
 python -m pip install transformers==4.37.0
 copy config_437.yaml ..\python\llm\dev\benchmark\all-in-one\config.yaml
 cd ..\python\llm\dev\benchmark\all-in-one
@@ -35,4 +43,5 @@ python run.py
 move *.csv "%OUTPUT_DIR%"
 cd %CWD%
 
-
+@REM concatenate all csv files
+@REM python concat_csv.py -i "%OUTPUT_DIR%"
